76766d5a0dc4023dc5bb5c54afe5c2183801e516
Lampppp <ldkoog00@harrisonburg.k12.va.us>
2019-09-16 09:05:46 -0400

    thetrheeeeeeeeepeiegges

--- sketch.js
+++ sketch.js
@@ -1,19 +1,20 @@
 var eatRabbit = false;
-var death = false;
+var breathDeath = false;
+var pigDeath = false;
 
 function setup() {
 
   // Change these to change the title and author of the story
-  document.getElementById('title').innerHTML="Goldilocks & the Three Bears"; // title
+  document.getElementById('title').innerHTML="The Bad Big Wolf and a Pig Maybe"; // title
 
-  document.getElementById('author').innerHTML="Jon Stapleton"; // author
+  document.getElementById('author').innerHTML="By: Me"; // author
 
 
   
   // All of your code goes under here
   
 
-  var story = 'The Big Bad Wolf is very hungry.' + food() + breath() + pig(); // Create the text
+  var story = 'The Big Bad Wolf is very hungry.' + food() + breath() + pig() + brick(); // Create the text
 
   printToPage(story); // Print the text to the webpage
 }
@@ -28,10 +29,10 @@ function food() {
     eatRabbit = true;
   }
   if(eatRabbit == true) {
-    return "So the wolf decided to eat a rabbit";
+    return " So the wolf decided to eat a rabbit.";
   } else {
     
-    return "As he was deciding on what he wanted to eat, he noticed an easy to blow down straw house with a nice juicy pig inside";
+    return " As he was deciding on what he wanted to eat, he noticed an easy to blow down straw house with a nice juicy pig inside.";
   }
   
 }
@@ -41,26 +42,46 @@ function food() {
 function breath() {
   if(eatRabbit == false){
     let breath = random(1, 6);
-    if(breath > 5) {
-      death = true;
-      return "The wolf walks up to the straw house, and he huffs, and puffs, and runs out of breath and dies."
+    if(breath < 4) {
+      breathDeath = true;
+      return " The wolf walks up to the straw house, and he huffs, and puffs, and runs out of breath and dies."
     } else {
-      return "The wolf walks up to the straw house, and he huffs, and puffs, and blows the house down."
+      return " The wolf walks up to the straw house, and he huffs, and puffs, and blows the house down!"
     }
+  } else {
+    return ""
   }
   
 }
 
 function pig() {
-  if(eatRabbit == false && death == false){
-    let death = random(1, 6);
-    if(death > 5) {
-      return "The wolf walks up to the straw house, and he huffs, and puffs, and runs out of breath and dies."
+  if(eatRabbit == false && breathDeath == false){
+    let pigDeath = random(1, 6);
+    if(pigDeath < 4) {
+      pigDeath = true;
+      return " Aftter he ate the pig, he finds a stick house with a nice juicy pig inside. So he huffed, and puffed, and the pig inside killed the wolf as revenge for eating his brother."
     } else {
-      return "The wolf walks up to the straw house, and he huffs, and puffs, and blows the house down."
+      return " Aftter he ate the pig, he finds a stick house with a nice juicy pig inside. So he huffs, and puffs, and blows the house down!"
     }
+  } else{
+    return ""
   }
   
+}
+
+function brick() {
+  if(eatRabbit == false && breathDeath == false && pigDeath == false){
+    let fireDeath = random(1, 6);
+    if(fireDeath > 3) {
+      return " Aftter he ate the pig, he finds a brick house with a nice juicy pig inside. so he climbed into the house through the chimney. But the pig inside heard the wolf, so he lit up the fireplace and the wolf burned to death."
+    } else if(fireDeath == 1) {
+      return " Aftter he ate the pig, he finds a brick house with a nice juicy pig inside. so he climbed into the house through the chimney. He was able to succesfully sneak into the house and ended up having three juicy, fat, stupid, ugly pigs for dinner."
+    } else{
+     return " Aftter he ate the pig, he finds a brick house with a nice juicy pig inside. So he huffs, and puffs, and runs out of breath and dies becuase you can't blow down a brick house."
+    }
+  } else {
+    return""
+  }
 }
 
 
